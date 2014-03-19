89d9be2489781f90166bf738b9b0ce2a9198ca76
Marion Ano <meaola@gmail.com>
2014-03-18 22:30:35 -0500

    first commit

---
+++ AdventureHanaHou/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AdventureHanaHou
+//
+//  Created by Marion Ano on 3/18/14.
+//  Copyright (c) 2014 Marion Ano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AdventureHanaHouTests/AdventureHanaHouTests.m
@@ -0,0 +1,34 @@
+//
+//  AdventureHanaHouTests.m
+//  AdventureHanaHouTests
+//
+//  Created by Marion Ano on 3/18/14.
+//  Copyright (c) 2014 Marion Ano. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AdventureHanaHouTests : XCTestCase
+
+@end
+
+@implementation AdventureHanaHouTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
